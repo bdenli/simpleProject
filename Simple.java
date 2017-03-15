3b43ef8d53985222f4407a4de6f8923c0bd5c139
bdenli <basakgdenli@gmail.com>
2017-03-15 15:15:00 -0400

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,29 @@
+package simplePackage;
+
+import java.util.Random;
+
+public class Simple {
+	
+	public static String[] getStringSet(){
+		
+		String[] strSet = {"Maybe...", "Definitely!", "Ask Again!", "Don't count on it", "Absolutely", "Of course!", "Nope!","Yup!" };
+		
+		return strSet;
+	}
+	
+	
+	public static String selectAnswer(){
+		
+		Random randomGenerator = new Random();
+		int random = randomGenerator.nextInt(getStringSet().length);
+		
+		return getStringSet()[random];
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(selectAnswer());
+
+	}
+
+}
